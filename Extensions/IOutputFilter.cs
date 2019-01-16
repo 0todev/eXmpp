34f81f42f3f1904feb40ffda06a04384af11d12e
Tatenda Caston Hove <tatenda@yellowtail.co.za>
2019-01-16 15:04:34 +0200

    init commit

---
+++ Extensions/IOutputFilter.cs
@@ -0,0 +1,15 @@
+﻿using Org.TatlaCas.Library.eXmpp.Core;
+
+namespace Org.TatlaCas.Library.eXmpp.Extensions {
+	/// <summary>
+	/// Represents an extension that filters outgoing stanzas.
+	/// </summary>
+	/// <typeparam name="T">The type of the stanzas to filter.</typeparam>
+	internal interface IOutputFilter<T> where T : Stanza {
+		/// <summary>
+		/// Invoked when a stanza is being sent.
+		/// </summary>
+		/// <param name="stanza">The stanza which is being sent.</param>
+		void Output(T stanza);
+	}
+}
